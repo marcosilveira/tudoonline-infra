729e7f2a760616780f29cebe817aa03934c89a4c
Marco Silveira <marcosilveira39@gmail.com>
2026-02-21 21:26:19 -0300

    outro arquivo de configuração do rabbitmq

---
+++ tudoonline-workers/k8s/config/config_rabbit.php
@@ -0,0 +1,10 @@
+<?php
+$rabbit_host = 'rabbitmq.production.svc.cluster.local';
+$rabbit_port = 5672;
+$rabbit_user = 'admin';
+$rabbit_password = 'adminpassword';
+$rabbit_vhost = 'production';
+
+
+
+?>

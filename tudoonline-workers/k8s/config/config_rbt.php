54510112ed59c90443ff9559837168c3f07c8df6
Marco Silveira <marcosilveira39@gmail.com>
2026-02-21 21:21:22 -0300

    fix(workers): update rabbitmq credentials to admin:adminpassword in config_rbt.php matching k8s secrets

--- tudoonline-workers/k8s/config/config_rbt.php
+++ tudoonline-workers/k8s/config/config_rbt.php
@@ -6,4 +6,4 @@ require_once '../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 
 //acesso dentro do kubernetes
-$connection = new AMQPStreamConnection('rabbitmq.production.svc.cluster.local', 5672, 'tudoonline_workers_production', '8AOh4DKOzA7cTpKHRZOssExPcijVxbdg', 'production');
+$connection = new AMQPStreamConnection('rabbitmq.production.svc.cluster.local', 5672, 'admin', 'adminpassword', 'production');
